c8e9e65b96bdc6fb2a4f522350859bfa52ec4a43
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-12-17 16:37:33 +0100

    work on restaruant route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   res.render('index', {
       title: 'title if needed',
       restaurant: 'ChIJAxbcKmaSlxIRXlys2hUXidQ',
-      place: req.body
+
   });
 
 });
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -5,9 +5,23 @@ router.get('/:ID', function(req, res, next) {
     res.render('restaurant', {
         title: 'title if needed',
         restaurant: 'ChIJAxbcKmaSlxIRXlys2hUXidQ',
-        place: res.body
+        //place: req.body
+        /*place: {
+            name: "my restaurant",
+            vicinity: "lalal land"
+        },*/
+        //name: req.body.place.name,
+        //place: req.body.place
+        place: 'my restaurant',
+        name: 'name',
+        vicinity: 'lalal land',
+        phone: 78787,
+        website: 'llala.com',
+        reviews: {
+            description: 'lala'
+        }
     });
-    console.log(res.body)
+    console.log(req.body)
 
 });
 module.exports = router;
