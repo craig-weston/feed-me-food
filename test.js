78e35cc6529a2a6d982222506328b157037d5fe8
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2017-12-18 15:22:34 +0100

    test for google places on back end

---
+++ test.js
@@ -0,0 +1,48 @@
+let pos = {
+    lat: 39.5696,
+    lng: 2.6502,
+};
+
+var googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyCVXZ0vhPliqPIvwSUaSvZJ9XmcoJKtXaM'
+});
+
+googleMapsClient.placesNearby({
+    location: pos,
+    radius: 500,
+    type: 'restaurant'
+}, function(err, response) {
+    if (!err) {
+        console.log(response.json.results);
+    }
+});
+
+router.get('/', function(req, res, next) {
+    let pos = {
+        lat: 39.5696,
+        lng: 2.6502,
+    };
+
+    var googleMapsClient = require('@google/maps').createClient({
+        key: 'AIzaSyCVXZ0vhPliqPIvwSUaSvZJ9XmcoJKtXaM'
+    });
+
+    googleMapsClient.placesNearby({
+        location: pos,
+        radius: 500,
+        type: 'restaurant'
+    }, function(err, response) {
+        if (!err) {
+            console.log(response.json.results);
+            res.render('index', {
+                title: 'title if needed',
+                restaurants: response.json.results,
+
+            });
+        }
+    });
+
+
+
+
+});
