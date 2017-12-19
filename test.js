2f26a7eba3ac3b9554996d76839cb36a1841f3b3
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2017-12-19 15:22:24 +0100

    more testing

--- test.js
+++ test.js
@@ -44,3 +44,44 @@ router.get('/', function(req, res, next) {
 });
 
 https://googlemaps.github.io/google-maps-services-js/docs/GoogleMapsClient.html
+
+
+map.pug
+each restaurant in restaurantSchema
+
+		meta(name='info' value={restaurants})
+
+		$('.iw-reviews').forEach(element => {
+		    const coordinates = element.data('coordinates')
+			element.data('description')
+
+			map.marker(coordinates).addTo(map)
+				.bindPopup('<h1>' + element.data('title') + '</h1>')
+				.openPopup()
+		});
+
+
+(function() {
+
+    $.get('/ajax/places')
+        .success( places => {
+            // ... call goolge maps
+    })
+
+})();
+
+
+each restaurant in restaurants
+						div(data-coordinates)
+							a.iw-reviews(data-long={restaurant.longitude} href=`/map/${restaurant.place_id}`) #{restaurant.name}
+					#loading.loading
+
+
+
+
+
+app.use('/', index);
+app.use('/ajax/places', function(req, res){
+    //const places = // call gmaps api
+    res.json([]);
+} );
