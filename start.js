6058a94463a7d91e78aab1f7f3ed1c8051bb21c4
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2018-01-07 13:35:29 +0100

    env file

--- start.js
+++ start.js
@@ -1,3 +1,5 @@
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
 // Start our app!
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
